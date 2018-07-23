ffffebf56bd6bd829ebae8cf1341d7297172be09
Mariam Elsayem <elsayem.mariam@gmail.com>
2018-07-23 14:00:21 +0300

    With Documentation.

--- find-base.js
+++ find-base.js
@@ -1,6 +1,12 @@
 const request = require("request");
 
 module.exports = {
+    /**
+     * A function for requestting a result through a given URL.
+     * @param {string} url
+     * @returns {Promise<*>}
+     * @throws {error} when the API request is rejected. //CHECK
+     */
     request: (url) => {
         return new Promise((resolve, reject) => {
             request(url, (error, response, body) => {
@@ -12,6 +18,13 @@ module.exports = {
             });
         });
     },
+    /**
+     * A function for parsing the returned result from the url into Json format.
+     * @param url
+     * @returns {Promise<*>}
+     * @throws {error} when the API request is rejected. //CHECK
+     * @throws {e} when the body fail to be parsed. //CHECK
+     */
     request_json: (url) => {
         return new Promise((resolve, reject) => {
             request(url, (error, response, body) => {
